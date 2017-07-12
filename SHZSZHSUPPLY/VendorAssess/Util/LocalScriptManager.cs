422303f79112bbdd700997a167236efde6e2070f
lzgmxd <lzgmxd@gmail.com>
2017-07-12 08:40:29 +0800

    添加LocalScriptManager (#13)
    * DAL层，TempVendor中的getID改为从As_Temp_Vendor表中查询；
    查询供应商编号修改为根据金额大小动态查询对应的vendor type id；
    VendorInfo页面隐藏临时id输入，动态获取临时供应商编号，添加messageBox检查输入并提示。新建完成后自动跳转
    
    * LocalScriptManager

---
+++ SHZSZHSUPPLY/VendorAssess/Util/LocalScriptManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SHZSZHSUPPLY.VendorAssess.Util
+{
+    public static class LocalScriptManager
+    {
+        /// <summary>
+        /// 处理js脚本
+        /// </summary>
+        /// <param name="mypage"></param>
+        /// <param name="strScript"></param>
+        /// <param name="ID"></param>
+        public static void CreateScript(System.Web.UI.Page mypage, string strScript, string ID)
+        {
+            string strscript = "<script language='javascript'>";
+            strscript += strScript;
+            strscript += "</script>";
+            if (!mypage.ClientScript.IsStartupScriptRegistered(ID))
+                mypage.ClientScript.RegisterStartupScript(mypage.GetType(), ID, strscript);
+        }
+    }
+}
