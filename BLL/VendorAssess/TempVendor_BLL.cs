9c73ab2174d1af706cbcb0ded5553306f339e4b6
lzgmxd <lzgmxd@gmail.com>
2017-09-04 18:37:53 +0800

    添加选择过期供应商

--- BLL/VendorAssess/TempVendor_BLL.cs
+++ BLL/VendorAssess/TempVendor_BLL.cs
@@ -29,6 +29,11 @@ namespace BLL
             return TempVendor_DAL.getUsed(tempVendorID, factoryName);
         }
 
+        /// <summary>
+        /// 选择所有/单厂，通过filter控制
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
         public static Dictionary<string, Dictionary<string, string[]>> readVendorInfo(bool filter)
         {
             Dictionary<string, Dictionary<string, string[]>> info = new Dictionary<string, Dictionary<string, string[]>>();
@@ -77,6 +82,11 @@ namespace BLL
             return info;
         }
 
+        /// <summary>
+        /// 选择ko对应的供应商
+        /// </summary>
+        /// <param name="employeeID"></param>
+        /// <returns></returns>
         public static Dictionary<string, Dictionary<string, string[]>> readVendorInfo(string employeeID)
         {
             Dictionary<string, Dictionary<string, string[]>> info = new Dictionary<string, Dictionary<string, string[]>>();
@@ -103,6 +113,54 @@ namespace BLL
                             t += 2;
                         }
                         info[item["Factory_Name"].ToString()].Add(item["Vendor_Type"].ToString(), nm);
+                    }
+                }
+            }
+            return info;
+        }
+
+        /// <summary>
+        /// 选择过期供应商
+        /// </summary>
+        /// <param name="employeeID">Employee</param>
+        /// <param name="overdue">是否只选择过期</param>
+        /// <returns></returns>
+        public static Dictionary<string, Dictionary<string, string[]>> readVendorInfo(string employeeID, bool overdue)
+        {
+            Dictionary<string, Dictionary<string, string[]>> info = new Dictionary<string, Dictionary<string, string[]>>();
+
+            DataTable dt = SelectEmployeeVendor_DAL.readVendorInfo(employeeID);
+            DataTable overDt = FileOverDue_DAL.getTempVendorID_All(employeeID);
+
+            List<string> list = (from DataRow dr in overDt.Rows select dr["Temp_Vendor_ID"].ToString()).ToList();
+
+            string tempVendorID, tempVendorName;
+
+            if (dt != null)
+            {
+                foreach (DataRow item in dt.Rows)
+                {
+                    if (!info.ContainsKey(item["Factory_Name"].ToString()))
+                    {
+                        info.Add(item["Factory_Name"].ToString(), new Dictionary<string, string[]>());
+                    }
+                    if (!info[item["Factory_Name"].ToString()].ContainsKey(item["Vendor_Type"].ToString()))
+                    {
+                        DataRow[] smlDr = dt.Select(String.Format("Factory_Name='{0}' and Vendor_Type='{1}'", item["Factory_Name"].ToString(), item["Vendor_Type"].ToString()));
+                        string[] nm = new string[smlDr.Length * 2];
+                        int t = 0;
+                        for (int i = 0; i < smlDr.Length; i++)
+                        {
+                            tempVendorID = smlDr[i]["Temp_Vendor_ID"].ToString();
+                            tempVendorName = smlDr[i]["Temp_Vendor_Name"].ToString();
+                            if (list.Contains(tempVendorID))
+                            {
+                                nm[t] = tempVendorName;
+                                nm[t + 1] = tempVendorID;
+                                t += 2;
+                            }
+                        }
+                        info[item["Factory_Name"].ToString()].Add(item["Vendor_Type"].ToString(), (from str in nm where str!=null select str).ToArray<string>());
                     }
                 }
             }
