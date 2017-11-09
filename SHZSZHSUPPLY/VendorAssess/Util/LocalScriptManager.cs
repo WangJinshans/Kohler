155ce9f3d9a23c3a0cf21ff430adcd3728d3d360
lzgmxd <lzgmxd@gmail.com>
2017-11-09 20:25:06 +0800

    脚本调用添加无ID方法，但是目前不可用

--- SHZSZHSUPPLY/VendorAssess/Util/LocalScriptManager.cs
+++ SHZSZHSUPPLY/VendorAssess/Util/LocalScriptManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI;
 
 namespace SHZSZHSUPPLY.VendorAssess.Util
 {
@@ -20,6 +21,20 @@ namespace SHZSZHSUPPLY.VendorAssess.Util
             strscript += "</script>";
             if (!mypage.ClientScript.IsStartupScriptRegistered(ID))
                 mypage.ClientScript.RegisterStartupScript(mypage.GetType(), ID, strscript);
+        }
+
+        /// <summary>
+        /// 暂时不可用，原因不明 2017年11月6日20:03:24
+        /// </summary>
+        /// <param name="mypage"></param>
+        /// <param name="strScript"></param>
+        internal static void CreateScript(Page mypage, string strScript)
+        {
+            string strscript = "<script language='javascript'>";
+            strscript += strScript;
+            strscript += "</script>";
+            if (!mypage.ClientScript.IsStartupScriptRegistered(strScript))
+                mypage.ClientScript.RegisterStartupScript(mypage.GetType(), strScript, strscript);
         }
     }
 }
